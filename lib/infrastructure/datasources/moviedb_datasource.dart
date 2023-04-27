0cb918d346b12090ab23dc79b266e505a342103d
Jonathan Axel <jonathanaxelnunez@gmail.com>
2023-04-27 13:31:15 -0600

    Fin primera seccion

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -7,7 +7,7 @@ import 'package:cinemapedia/domain/entities/movie.dart';
 
 class MovieDbDataSource extends MoviesDataSource {
   final dio = Dio(BaseOptions(
-      baseUrl: 'https://api.themovie.org/3',
+      baseUrl: 'https://api.themoviedb.org/3',
       queryParameters: {
         'api_key': Environment.movieDbKey,
         'language': 'es-MX'
