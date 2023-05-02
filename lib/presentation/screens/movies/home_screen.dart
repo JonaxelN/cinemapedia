7c999608b5d8247a4fb3d515b70dc9dced1f95e7
Jonathan Axel <jonathanaxelnunez@gmail.com>
2023-05-02 13:11:35 -0600

    SplashScreenLoading

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
 import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
 import 'package:cinemapedia/presentation/providers/movies/movies_slideshow_provider.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/presentation/widgets/shared/full_screen_loader.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -37,63 +39,64 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
   @override
   Widget build(BuildContext context) {
+    final initialLoading = ref.watch(initialLoadingProvider);
+    if (initialLoading) return const FullScreenLoader();
+
     final nowPlaying6Movies = ref.watch(moviesSlideShowProvider);
+
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
     final popularMovies = ref.watch(popularMoviesProvider);
     final upComingMovies = ref.watch(upComingMoviesProvider);
     final bestRatedMovies = ref.watch(bestRatedMoviesProvider);
 
-    if (nowPlaying6Movies.isEmpty) {
-      return const Center(
-        child: CircularProgressIndicator(),
-      );
-    }
-
-    return CustomScrollView(slivers: [
-      const SliverAppBar(
-        floating: true,
-        flexibleSpace: FlexibleSpaceBar(
-          title: CustomAppBar(),
-          centerTitle: false,
-          titlePadding: EdgeInsets.zero,
+    return Visibility(
+      visible: !initialLoading,
+      child: CustomScrollView(slivers: [
+        const SliverAppBar(
+          floating: true,
+          flexibleSpace: FlexibleSpaceBar(
+            title: CustomAppBar(),
+            centerTitle: false,
+            titlePadding: EdgeInsets.zero,
+          ),
         ),
-      ),
-      SliverList(
-          delegate: SliverChildBuilderDelegate((context, index) {
-        return Column(
-          children: [
-            MoviesSlideShow(movies: nowPlaying6Movies),
-            MovieHorizontalListView(
-              movies: nowPlayingMovies,
-              title: 'En Cines',
-              subtitle: 'Lunes 20',
-              loadNextPage: () =>
-                  ref.read(nowPlayingMoviesProvider.notifier).loadNextPage(),
-            ),
-            MovieHorizontalListView(
-              movies: upComingMovies,
-              title: 'Próximamente',
-              subtitle: 'En Este Mes',
-              loadNextPage: () =>
-                  ref.read(upComingMoviesProvider.notifier).loadNextPage(),
-            ),
-            MovieHorizontalListView(
-              movies: popularMovies,
-              title: 'Populares',
-              // subtitle: 'Lunes 20',
-              loadNextPage: () =>
-                  ref.read(popularMoviesProvider.notifier).loadNextPage(),
-            ),
-            MovieHorizontalListView(
-              movies: bestRatedMovies,
-              title: 'Mejores Calificadas',
-              // subtitle: 'Lunes 20',
-              loadNextPage: () =>
-                  ref.read(bestRatedMoviesProvider.notifier).loadNextPage(),
-            ),
-          ],
-        );
-      }, childCount: 1))
-    ]);
+        SliverList(
+            delegate: SliverChildBuilderDelegate((context, index) {
+          return Column(
+            children: [
+              MoviesSlideShow(movies: nowPlaying6Movies),
+              MovieHorizontalListView(
+                movies: nowPlayingMovies,
+                title: 'En Cines',
+                subtitle: 'Lunes 20',
+                loadNextPage: () =>
+                    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage(),
+              ),
+              MovieHorizontalListView(
+                movies: upComingMovies,
+                title: 'Próximamente',
+                subtitle: 'En Este Mes',
+                loadNextPage: () =>
+                    ref.read(upComingMoviesProvider.notifier).loadNextPage(),
+              ),
+              MovieHorizontalListView(
+                movies: popularMovies,
+                title: 'Populares',
+                // subtitle: 'Lunes 20',
+                loadNextPage: () =>
+                    ref.read(popularMoviesProvider.notifier).loadNextPage(),
+              ),
+              MovieHorizontalListView(
+                movies: bestRatedMovies,
+                title: 'Mejores Calificadas',
+                // subtitle: 'Lunes 20',
+                loadNextPage: () =>
+                    ref.read(bestRatedMoviesProvider.notifier).loadNextPage(),
+              ),
+            ],
+          );
+        }, childCount: 1))
+      ]),
+    );
   }
 }
