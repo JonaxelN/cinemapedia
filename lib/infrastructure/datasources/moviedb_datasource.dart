20f81672d9dce4a7f4ae47efb315793cdf3955ff
Jonathan Axel <jonathanaxelnunez@gmail.com>
2023-05-09 11:39:42 -0600

    Mostrar la búsqueda http en el search delegate

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -61,4 +61,11 @@ class MovieDbDataSource extends MoviesDataSource {
     final Movie movie = MovieMapper.movieDetailsToEntoty(movieDb);
     return movie;
   }
+
+  @override
+  Future<List<Movie>> searchMovies(String query) async {
+    final response =
+        await dio.get('/search/movie', queryParameters: {'query': query});
+    return _jsonToMovies(response.data!);
+  }
 }
